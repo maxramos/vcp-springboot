a1564e5f7def78b08e5083cd298a5db073973c30
Max <Max@MAX-LENOVO>
2022-06-05 20:59:14 +0800

    Added some pointcuts.

--- src/main/java/ph/mramos/vcps/section09/aspect/LoggerAspect.java
+++ src/main/java/ph/mramos/vcps/section09/aspect/LoggerAspect.java
@@ -17,6 +17,7 @@ import org.springframework.stereotype.Component;
 @Component
 public class LoggerAspect {
 
+//	@Pointcut("execution(public ph.mramos.vcps.section09.entity.Person ph.mra*.vcps..*Service.find*(..))")
 	@Pointcut("execution(* ph.mramos.vcps.section09.service.*Service.*(..))")
 	private void services() {}
 
