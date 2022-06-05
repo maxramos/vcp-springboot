299c36aab7fd41f573ee23b98a7183e1fda53051
Max <Max@MAX-LENOVO>
2022-06-05 20:02:34 +0800

    Added comments.

--- src/main/java/ph/mramos/vcps/section09/Application.java
+++ src/main/java/ph/mramos/vcps/section09/Application.java
@@ -6,6 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication // Default entity scanning starts where the @EnableAutoConfiguration / @SpringBootApplication is located.
 // @EntityScan // Can be specified in a different class to that scanning starts there.
 // @EnableJpaRepositories // No need to define this since all repositories will be auto detected from where the @EnableAutoConfiguration / @SpringBootApplication is located.
+// @EnableAspectJAutoProxy // No need to define this since if aspect related jars are in the classpath then this annotation will be auto configured.
+						   // Note that @EnableAspectJAutoProxy is per app context so if there a seprate servlet web app context then a separate annotation is required if the other annotation is declared on the root context.
 public class Application {
 
 	public static void main(String[] args) {
