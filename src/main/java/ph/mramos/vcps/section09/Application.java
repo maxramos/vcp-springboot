b6a3ad79b98bdadb251cfff7b67717e58d53d2e1
Max <Max@MAX-LENOVO>
2022-06-13 00:37:37 +0800

    Added transacion comment.

--- src/main/java/ph/mramos/vcps/section09/Application.java
+++ src/main/java/ph/mramos/vcps/section09/Application.java
@@ -8,6 +8,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 // @EnableJpaRepositories // No need to define this since all repositories will be auto detected from where the @EnableAutoConfiguration / @SpringBootApplication is located.
 // @EnableAspectJAutoProxy // No need to define this since if aspect related jars are in the classpath then this annotation will be auto configured.
 						   // Note that @EnableAspectJAutoProxy is per app context so if there a seprate servlet web app context then a separate annotation is required if the other annotation is declared on the root context.
+//@EnableTransactionManagement // No need to define this. Transaction will be auto configured.
 public class Application {
 
 	public static void main(String[] args) {
